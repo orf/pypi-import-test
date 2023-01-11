4794653a995757bdcea3eb1e08deebe9840f302c
Tom Forbes <tom@tomforb.es>
2023-01-11 16:15:42 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,98 @@
 use flate2::read::GzDecoder;
 use rayon::prelude::*;
-use sha2::{Digest};
+use sha2::{Digest, Sha256};
 use std::ffi::OsStr;
 use std::fs::{DirEntry, File};
 use std::io::{BufReader, Read};
 use std::io::Write;
-use std::{env, fs};
-
+use std::{env, fs, io};
+use std::time::Duration;
 use tar::Archive;
 use indicatif::{ParallelProgressIterator, ProgressStyle};
 use indicatif::ProgressBar;
-use content_inspector::{inspect};
+use content_inspector::{ContentType, inspect};
+
+use std::path::{Path, PathBuf};
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg()]
+    input: PathBuf,
+
+    #[arg()]
+    output: PathBuf
+}
 
 const BUF_SIZE: usize = 1024 * 1024 * 32;
 
 fn main() -> anyhow::Result<()> {
-    let dir = env::args().last().unwrap();
-    let all_entries: Vec<DirEntry> = fs::read_dir(dir)?.flatten().collect();
+    let args: Cli = Cli::parse();
+    let all_entries: Vec<DirEntry> = fs::read_dir(args.input)?.flatten().collect();
+
     let size = all_entries.len();
     let pbar = ProgressBar::new(size as u64);
+
     // pbar.enable_steady_tick(Duration::from_secs(1));
     let style = ProgressStyle::with_template("{prefix:>12.cyan.bold} [{bar:57}] {pos}/{len} ({eta})").unwrap();
     pbar.set_style(style);
     all_entries.into_par_iter().progress_with(pbar).for_each(|entry| {
         let path = entry.path();
-        let results: Vec<_> = match path.extension().and_then(OsStr::to_str) {
+        match path.extension().and_then(OsStr::to_str) {
             Some(ext) => match ext {
                 "gz" => {
                     let tar_gz = BufReader::with_capacity(BUF_SIZE, File::open(path).unwrap());
                     let tar = GzDecoder::new(tar_gz);
                     let mut archive = Archive::new(tar);
-                    archive
-                        .entries()
-                        .unwrap()
-                        .flatten()
-                        .filter(|v| v.size() != 0)
-                        .map(|entry| {
-                            // io::copy(&mut entry, &mut hasher).unwrap();
-                            // let result = hasher.finalize_reset();
-                            let extension = {
-                                let x = &entry.path().unwrap();
-                                x
-                                    .extension()
-                                    .and_then(|v| v.to_str()).map(|v| v.to_string())
-                                    .unwrap_or_else(||"None".to_string())
-                            };
-                            let size = entry.size();
-                            let mut buf = Vec::with_capacity(1024);
-                            let mut first_bytes = entry.take(1024);
-                            first_bytes.read_to_end(&mut buf).unwrap();
-                            let res = inspect(&buf);
-                            Some(format!("{size} {extension} {res}"))
-                        })
-                        .collect()
+                    let mut hasher = Sha256::new();
+
+                    for mut entry in archive.entries().unwrap().flatten().filter(|v| v.size() != 0) {
+                        let mut buf = Vec::with_capacity(entry.size() as usize);
+                        entry.read_to_end(&mut buf).unwrap();
+                        let content_type = inspect(&buf);
+
+                        if content_type == ContentType::BINARY {
+                            continue;
+                        }
+                        hasher.update(&buf);
+                        let result = hasher.finalize_reset();
+                        let file_name = format!("{result:X}");
+                        let output_path = args.output.join(&file_name[0..4]).join(file_name);
+                        fs::create_dir_all(output_path.parent().unwrap()).unwrap();
+                        fs::write(output_path, buf).unwrap();
+                    }
                 }
                 "egg" | "zip" | "whl" => {
                     let zip_file = BufReader::with_capacity(BUF_SIZE, File::open(path).unwrap());
+                    let mut hasher = Sha256::new();
                     match zip::ZipArchive::new(zip_file) {
                         Ok(mut archive) => (0..archive.len())
                             .map(|i| {
-                                let file = archive.by_index(i).unwrap();
+                                let mut file = archive.by_index(i).unwrap();
                                 if !file.is_file() || file.size() == 0 {
                                     return None;
                                 }
+                                io::copy(&mut file, &mut hasher).unwrap();
+                                let result = hasher.finalize_reset();
                                 let extension = match file.enclosed_name() {
                                     Some(v) => {
                                         v.extension().and_then(|v| v.to_str()).unwrap_or("None")
                                     }
                                     None => "None",
-                                }.to_string();
-                                let size = file.size();
-                                let mut buf = Vec::with_capacity(1024);
-                                let mut first_bytes = file.take(1024);
-                                first_bytes.read_to_end(&mut buf).unwrap();
-                                // io::copy(&mut file, &mut hasher).unwrap();
-                                // let result = hasher.finalize_reset();
-                                let res = inspect(&buf);
-                                Some(format!("{size} {extension} {res}"))
+                                };
+                                Some(format!("{result:X} {} {}", file.size(), extension))
                             })
                             .collect(),
-                        Err(_) => vec![],
+                        Err(_) => {}
                     }
                 }
                 _ => panic!("Unhandled extension {ext}"),
+                _ => {},
             },
-            None => vec![],
+            None => {}
         };
-        if results.is_empty() {
-            return;
-        }
-        let stdout = std::io::stdout();
-        let mut lock = stdout.lock();
-        for line in results.iter().flatten() {
-            writeln!(lock, "{line}").unwrap();
-        }
     });
     Ok(())
 }
