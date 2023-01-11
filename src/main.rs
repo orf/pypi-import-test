d6e7f33a776413fcb5a0bb74f819acdb1fde3161
Tom Forbes <tom@tomforb.es>
2023-01-11 14:38:59 +0000

    Without hash

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use flate2::read::GzDecoder;
 use rayon::prelude::*;
-use sha2::{Digest, Sha256};
+use sha2::{Digest};
 use std::ffi::OsStr;
 use std::fs::{DirEntry, File};
-use std::io::BufReader;
+use std::io::{BufReader, Read};
 use std::io::Write;
-use std::{env, fs, io};
-use std::time::Duration;
+use std::{env, fs};
+
 use tar::Archive;
 use indicatif::{ParallelProgressIterator, ProgressStyle};
 use indicatif::ProgressBar;
+use content_inspector::{inspect};
 
 const BUF_SIZE: usize = 1024 * 1024 * 32;
 
@@ -29,43 +30,53 @@ fn main() -> anyhow::Result<()> {
                     let tar_gz = BufReader::with_capacity(BUF_SIZE, File::open(path).unwrap());
                     let tar = GzDecoder::new(tar_gz);
                     let mut archive = Archive::new(tar);
-                    let mut hasher = Sha256::new();
                     archive
                         .entries()
                         .unwrap()
                         .flatten()
                         .filter(|v| v.size() != 0)
-                        .map(|mut entry| {
-                            io::copy(&mut entry, &mut hasher).unwrap();
-                            let result = hasher.finalize_reset();
-                            let entry_path = entry.path().unwrap();
-                            let extension = entry_path
-                                .extension()
-                                .and_then(|v| v.to_str())
-                                .unwrap_or("None");
-                            Some(format!("{result:X} {} {}", entry.size(), extension))
+                        .map(|entry| {
+                            // io::copy(&mut entry, &mut hasher).unwrap();
+                            // let result = hasher.finalize_reset();
+                            let extension = {
+                                let x = &entry.path().unwrap();
+                                x
+                                    .extension()
+                                    .and_then(|v| v.to_str()).map(|v| v.to_string())
+                                    .unwrap_or_else(||"None".to_string())
+                            };
+                            let size = entry.size();
+                            let mut buf = Vec::with_capacity(1024);
+                            let mut first_bytes = entry.take(1024);
+                            first_bytes.read_to_end(&mut buf).unwrap();
+                            let res = inspect(&buf);
+                            Some(format!("{size} {extension} {res}"))
                         })
                         .collect()
                 }
                 "egg" | "zip" | "whl" => {
-                    let zip_file = BufReader::with_capacity(BUF_SIZE,File::open(path).unwrap());
-                    let mut hasher = Sha256::new();
+                    let zip_file = BufReader::with_capacity(BUF_SIZE, File::open(path).unwrap());
                     match zip::ZipArchive::new(zip_file) {
                         Ok(mut archive) => (0..archive.len())
                             .map(|i| {
-                                let mut file = archive.by_index(i).unwrap();
+                                let file = archive.by_index(i).unwrap();
                                 if !file.is_file() || file.size() == 0 {
-                                    return None
+                                    return None;
                                 }
-                                io::copy(&mut file, &mut hasher).unwrap();
-                                let result = hasher.finalize_reset();
                                 let extension = match file.enclosed_name() {
                                     Some(v) => {
                                         v.extension().and_then(|v| v.to_str()).unwrap_or("None")
                                     }
                                     None => "None",
-                                };
-                                Some(format!("{result:X} {} {}", file.size(), extension))
+                                }.to_string();
+                                let size = file.size();
+                                let mut buf = Vec::with_capacity(1024);
+                                let mut first_bytes = file.take(1024);
+                                first_bytes.read_to_end(&mut buf).unwrap();
+                                // io::copy(&mut file, &mut hasher).unwrap();
+                                // let result = hasher.finalize_reset();
+                                let res = inspect(&buf);
+                                Some(format!("{size} {extension} {res}"))
                             })
                             .collect(),
                         Err(_) => vec![],
