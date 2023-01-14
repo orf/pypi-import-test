533fa87db770c6e8651fe8aa2d4e1124c8442112
Tom Forbes <tom@tomforb.es>
2023-01-14 20:30:42 +0000

    remove -0n

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ export CONCURRENCY="$2"
 export INPUT="$3"
 export INPUT_BASE=$(basename "$INPUT")
 
-parallel -a"$INPUT" --progress --eta -n200 -P"$CONCURRENCY" --joblog=logs/"${INPUT_BASE}".log ./target/release/pypi-import-test --repo="$REPO" from-json
+parallel -a"$INPUT" --progress --eta -P"$CONCURRENCY" --joblog=logs/"${INPUT_BASE}".log ./target/release/pypi-import-test --repo="$REPO" from-json
