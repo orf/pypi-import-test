d25828f4dab0e2af8d6c8b8ba6358c3031d056c4
Tom Forbes <tom@tomforb.es>
2023-02-07 16:39:06 +0000

    Sort the indexes? Maybe improves compression?

--- combine_workspace.sh
+++ combine_workspace.sh
@@ -16,7 +16,7 @@ mkdir -p "$WORKSPACE"
 mkdir -p "$COMBINED_DIR"
 
 echo "Creating step index"
-fd -a . "$INPUT_GIT_DIR" | shuf > "$INDEX_FILE"
+fd -a . "$INPUT_GIT_DIR" | sort > "$INDEX_FILE"
 
 export RUST_LOG=warn
 parallel -u --progress --eta --joblog="$WORKSPACE"/job.log --xargs -n"$PACKAGES_PER_PARTITION" -P"$CONCURRENCY" -a "$INDEX_FILE" -I{} "./target/release/pypi-import-test combine {#} ${COMBINED_DIR}/{#}/ {} 2>&1"
