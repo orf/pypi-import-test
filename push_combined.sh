750fa780853972b6af9d104f065da33587e24c94
Tom Forbes <tom@tomforb.es>
2023-03-03 10:44:16 +0000

    ok

--- push_combined.sh
+++ push_combined.sh
@@ -7,4 +7,4 @@ export COMBINED_DIR="$1"
 export PROCESSES="$2"
 export RUST_LOG=warn
 
-fd --max-depth=1 -t=d . "$COMBINED_DIR" -X "printf" '%s\n' '{/}' | sort -h | head -n30 | parallel -u --progress --eta --xargs -N5 -P"$PROCESSES" -I@ "./target/release/pypi-import-test create-repository @ && git -C @ push origin main:main && git -C @ push origin import:import 2>&1"
+fd --max-depth=1 -t=d . "$COMBINED_DIR" -X "printf" '%s\n' '{/}' | sort -h | head -n30 | parallel -u --progress --eta --xargs -N1 -P"$PROCESSES" -I@ "./target/release/pypi-import-test create-repository $COMBINED_DIR/@ && git -C @ push origin main:main && git -C @ push origin import:import 2>&1"
