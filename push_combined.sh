b4eff31bdef6508d5605802ceb08fa94bfeb34b1
Tom Forbes <tom@tomforb.es>
2023-03-05 23:00:13 +0000

    Remove head

--- push_combined.sh
+++ push_combined.sh
@@ -7,4 +7,4 @@ export COMBINED_DIR="$1"
 export PROCESSES="$2"
 export RUST_LOG=warn
 
-fd --max-depth=1 -t=d . "$COMBINED_DIR" -X "printf" '%s\n' '{/}' | sort -h | head -n30 | parallel -u --progress --eta --xargs -N1 -P"$PROCESSES" -I@ "./target/release/pypi-import-test create-repository $COMBINED_DIR/@ && git -C $COMBINED_DIR/@ push --force origin main:main && git -C $COMBINED_DIR/@ push --force origin import:import 2>&1"
+fd --max-depth=1 -t=d . "$COMBINED_DIR" -X "printf" '%s\n' '{/}' | sort -h | parallel -u --progress --eta --xargs -N1 -P"$PROCESSES" -I@ "./target/release/pypi-import-test create-repository $COMBINED_DIR/@ && git -C $COMBINED_DIR/@ push --force origin main:main && git -C $COMBINED_DIR/@ push --force origin import:import 2>&1"
